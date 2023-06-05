a9d2549b6d6a7e62895dada45803ae7bd2a62ee6
VanesaMeze <mezovanessza39@gmail.com>
2023-06-05 11:50:21 +0200

    Prvi zadatak

---
+++ database/migrations/2023_06_05_094327_create_cars_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string("brand");
+            $table->string("model");
+            $table->integer("year");
+            $table->integer("max_speed");
+            $table->boolean("is_automatic");
+            $table->string("engine");
+            $table->integer("number_of_doors");
+            $table->date("created_at");
+            $table->date("updated_at");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
